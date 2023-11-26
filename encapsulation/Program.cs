8550c581a6090b62a1422d361d3d556f9ca351c0
Imranhkh <imranpmot@gmail.com>
2023-11-26 13:31:31 +0600

    today

---
+++ encapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿public class EmpoyeeGetSet
+{
+    private string Name;
+    private string Salary;
+    private DateTime created;
+
+    public void display()
+    {
+        Console.WriteLine(this.Name);
+    }
+    public string NAME
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        EmpoyeeGetSet Emp = new EmpoyeeGetSet();
+        Emp.NAME = "pranto";
+        Emp.display();
+        Console.ReadLine();
+    }
+
+}
---
+++ vrrrrrrrr/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var Ab = new[]
+{
+            new {ID =14172 ,Name="Pranto",Mobile="01752335210"},
+            new {ID =13926 ,Name="surovi",Mobile="01752335210"},
+            new {ID =14174 ,Name="tausif",Mobile="01752335210"},
+        };
+        foreach (var item in Ab)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
